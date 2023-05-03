fe850b32503ec3116fec4504183011f8634f03b3
Riikka Alanen <riikka.alanen@kuubi.fi>
2023-05-03 20:56:01 +0300

    Add pins for all eye colors

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 import RPi.GPIO as GPIO
 import time
 
-LED_PIN = 17
+EYE_L_R = 22
+EYE_L_G = 27
+EYE_L_B = 17
+EYE_R_R = 23
+EYE_R_G = 24
+EYE_R_B = 25
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(EYE_L_R, GPIO.OUT)
+GPIO.setup(EYE_L_G, GPIO.OUT)
+GPIO.setup(EYE_L_B, GPIO.OUT)
+GPIO.setup(EYE_R_R, GPIO.OUT)
+GPIO.setup(EYE_R_G, GPIO.OUT)
+GPIO.setup(EYE_R_B, GPIO.OUT)
 
 # LED päälle
-GPIO.output(LED_PIN, GPIO.HIGH)
-
+GPIO.output(EYE_L_R, GPIO.HIGH)
 time.sleep(1)
+GPIO.output(EYE_L_R, GPIO.LOW)
 
-# LED pois
-GPIO.output(LED_PIN, GPIO.LOW)
+GPIO.output(EYE_L_G, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(EYE_L_G, GPIO.LOW)
+
+GPIO.output(EYE_L_B, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(EYE_L_B, GPIO.LOW)
+
+GPIO.output(EYE_R_R, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(EYE_R_R, GPIO.LOW)
+
+GPIO.output(EYE_R_G, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(EYE_R_G, GPIO.LOW)
+
+GPIO.output(EYE_R_B, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(EYE_R_B, GPIO.LOW)
 
 
 GPIO.cleanup()
