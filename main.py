bbce31a86a57e27188e63b3e99483fd51e7ac036
Riikka Alanen <riikka.alanen@kuubi.fi>
2023-05-09 19:37:28 +0300

    add query parameters and hex

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 import RPi.GPIO as GPIO
-from flask import Flask
+from flask import Flask, request
 
 def shutDown():
     GPIO.cleanup()
@@ -20,34 +20,61 @@ GPIO.setup(EYE_R_R, GPIO.OUT)
 GPIO.setup(EYE_R_G, GPIO.OUT)
 GPIO.setup(EYE_R_B, GPIO.OUT)
 
+left_red_pwm = GPIO.PWM(EYE_L_R, 1000)
+left_green_pwm = GPIO.PWM(EYE_L_G, 1000)
+left_blue_pwm = GPIO.PWM(EYE_L_B, 1000)
+right_red_pwm = GPIO.PWM(EYE_R_R, 1000)
+right_green_pwm = GPIO.PWM(EYE_R_G, 1000)
+right_blue_pwm = GPIO.PWM(EYE_R_B, 1000)
+
+left_red_pwm.start(0)
+left_green_pwm.start(0)
+left_blue_pwm.start(0)
+right_red_pwm.start(0)
+right_green_pwm.start(0)
+right_blue_pwm.start(0)
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
 
-    # LED päälle
-    GPIO.output(EYE_L_R, GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(EYE_L_R, GPIO.LOW)
+    left_color = request.args.get("left", default="000000")
+    right_color = request.args.get("right", default="000000")
 
-    GPIO.output(EYE_L_G, GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(EYE_L_G, GPIO.LOW)
+    # Parse hex color strings to integers
+    left_red = int(left_color[0:2], 16)
+    left_green = int(left_color[2:4], 16)
+    left_blue = int(left_color[4:6], 16)
+    right_red = int(right_color[0:2], 16)
+    right_green = int(right_color[2:4], 16)
+    right_blue = int(right_color[4:6], 16)
 
-    GPIO.output(EYE_L_B, GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(EYE_L_B, GPIO.LOW)
+    # Normalize RGB values to 0-100 range
+    left_red_normalized = (left_red / 255.0) * 100
+    left_green_normalized = (left_green / 255.0) * 100
+    left_blue_normalized = (left_blue / 255.0) * 100
+    right_red_normalized = (right_red / 255.0) * 100
+    right_green_normalized = (right_green / 255.0) * 100
+    right_blue_normalized = (right_blue / 255.0) * 100
 
-    GPIO.output(EYE_R_R, GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(EYE_R_R, GPIO.LOW)
+    # Set PWM duty cycle for left eye
+    left_red_pwm.ChangeDutyCycle(left_red_normalized)
+    left_green_pwm.ChangeDutyCycle(left_green_normalized)
+    left_blue_pwm.ChangeDutyCycle(left_blue_normalized)
 
-    GPIO.output(EYE_R_G, GPIO.HIGH) 
-    time.sleep(1)
-    GPIO.output(EYE_R_G, GPIO.LOW)
+    # Set PWM duty cycle for right eye
+    right_red_pwm.ChangeDutyCycle(right_red_normalized)
+    right_green_pwm.ChangeDutyCycle(right_green_normalized)
+    right_blue_pwm.ChangeDutyCycle(right_blue_normalized)
 
-    GPIO.output(EYE_R_B, GPIO.HIGH)
+    # Sleep for 1 second
     time.sleep(1)
-    GPIO.output(EYE_R_B, GPIO.LOW)
-    return "<p>Hello, World!</p>"
+
+    # Turn off all LEDs
+    left_red_pwm.ChangeDutyCycle(0)
+    left_green_pwm.ChangeDutyCycle(0)
+    left_blue_pwm.ChangeDutyCycle(0)
+    right_red_pwm.ChangeDutyCycle(0)
+    return "<p>Knock knock.</p><p>Who’s there? </p><p>Art</p><p>Art who?</p><p>R2D2</p>"
