726135fe9435edf80ab22bfbdd81bd7ff166e9df
Riikka Alanen <riikka.alanen@kuubi.fi>
2023-05-09 18:28:45 +0300

    Flask with hello world

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import RPi.GPIO as GPIO
 import time
+import RPi.GPIO as GPIO
+from flask import Flask
+
+def shutDown():
+    GPIO.cleanup()
 
 EYE_L_R = 22
 EYE_L_G = 27
@@ -16,30 +20,34 @@ GPIO.setup(EYE_R_R, GPIO.OUT)
 GPIO.setup(EYE_R_G, GPIO.OUT)
 GPIO.setup(EYE_R_B, GPIO.OUT)
 
-# LED päälle
-GPIO.output(EYE_L_R, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(EYE_L_R, GPIO.LOW)
-
-GPIO.output(EYE_L_G, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(EYE_L_G, GPIO.LOW)
-
-GPIO.output(EYE_L_B, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(EYE_L_B, GPIO.LOW)
-
-GPIO.output(EYE_R_R, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(EYE_R_R, GPIO.LOW)
-
-GPIO.output(EYE_R_G, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(EYE_R_G, GPIO.LOW)
-
-GPIO.output(EYE_R_B, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(EYE_R_B, GPIO.LOW)
+app = Flask(__name__)
 
 
-GPIO.cleanup()
+@app.route("/")
+def hello_world():
+
+    # LED päälle
+    GPIO.output(EYE_L_R, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(EYE_L_R, GPIO.LOW)
+
+    GPIO.output(EYE_L_G, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(EYE_L_G, GPIO.LOW)
+
+    GPIO.output(EYE_L_B, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(EYE_L_B, GPIO.LOW)
+
+    GPIO.output(EYE_R_R, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(EYE_R_R, GPIO.LOW)
+
+    GPIO.output(EYE_R_G, GPIO.HIGH) 
+    time.sleep(1)
+    GPIO.output(EYE_R_G, GPIO.LOW)
+
+    GPIO.output(EYE_R_B, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(EYE_R_B, GPIO.LOW)
+    return "<p>Hello, World!</p>"
